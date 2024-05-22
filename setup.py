1e830795c98455ac8db2dd55e3dc713f6a268189
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2024-05-22 23:44:51 +0000

    chore: Refactor module imports and remove unused utility function

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 DESCRIPTION = 'Python packages for mlab ecosystem'
 LONG_DESCRIPTION = 'This package contains supports the core functionalities for the mlab ecosystem'
 
